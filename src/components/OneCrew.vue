ead921090151ee9671ec28c9642d2b75f6079b46
LazezBZH <lazez.bzh@caramail.fr>
2022-05-29 11:44:04 +0200

    V30crew part1 desk

---
+++ src/components/OneCrew.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="data[i].name" class="crew">
+    <h3 class="role">{{ data[i].role.toUpperCase() }}</h3>
+    <h4 class="name">{{ data[i].name.toUpperCase() }}</h4>
+    <p class="bio">
+      {{ data[i].bio }}
+    </p>
+  </div>
+</template>
+
+<script>
+import json from "../data.json";
+let data = json.crew;
+console.log(data);
+
+export default {
+  name: "OneCrew",
+  props: {
+    i: {},
+  },
+  data() {
+    return {
+      data,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.crew {
+  color: white;
+  width: 80%;
+  margin-left: 0;
+  margin-top: 0;
+}
+.image {
+  width: 10rem;
+}
+h3 {
+  font-family: "Noto Serif", serif;
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: rgba(255, 255, 255, 0.5);
+}
+h4 {
+  font-family: "Noto Serif", serif;
+  font-size: 2.62rem;
+  font-weight: 400;
+  margin: 1.3rem auto;
+}
+.bio {
+  font-family: "Barlow", sans-serif;
+  font-size: 1.2rem;
+  margin: 1rem auto 2rem auto;
+  padding-right: 2.1rem;
+  color: rgba(208, 214, 249, 0.9);
+  line-height: 1.8rem;
+}
+</style>
--- src/views/CrewView.vue
+++ src/views/CrewView.vue
@@ -1,5 +1,89 @@
 <template>
-  <div class="crew">
-    <h1>This is an crew page</h1>
+  <div class="crews">
+    <h1><span>02</span> MEET YOUR CREW</h1>
+    <div class="crew-txt">
+      <div class="crew-all">
+        <OneCrew :i="activeId"></OneCrew>
+
+        <button
+          v-for="item in data"
+          :key="item.name"
+          v-on:click="activeId = data.indexOf(item)"
+        ></button>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import json from "../data.json";
+let data = json.crew;
+
+console.log(data);
+
+import OneCrew from "@/components/OneCrew.vue";
+
+export default {
+  name: "CrewView",
+  components: { OneCrew },
+  data() {
+    return {
+      data,
+      activeId: 0,
+    };
+  },
+};
+</script>
+<style scoped>
+.crews {
+  max-width: 1440px;
+  padding-top: 17%;
+  width: 100%;
+  height: 100vh;
+  background-image: url("@/assets/crew/background-crew-desktop.jpg");
+  background-size: auto;
+  background-repeat: no-repeat;
+  background-position: top right;
+}
+h1 {
+  color: white;
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 1.7rem;
+  letter-spacing: 4.72px;
+  font-weight: 400;
+  margin-left: 10rem;
+}
+h1 span {
+  padding-right: 2rem;
+  color: rgba(255, 255, 255, 0.4);
+  font-weight: 700;
+}
+.crew-txt {
+  width: 57%;
+  height: 60vh;
+  margin-left: 0;
+  position: relative;
+}
+.crew-all {
+  width: 75%;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding-bottom: 2rem;
+}
+button {
+  width: 0.7rem;
+  height: 0.7rem;
+  background-color: white;
+  color: #d0d6f9;
+  border: none;
+  padding: 0;
+  margin: 3rem 2rem 0 0;
+  border-radius: 50%;
+}
+h2 {
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 1.5rem;
+  font-weight: 400;
+  letter-spacing: 2.7px;
+}
+</style>
