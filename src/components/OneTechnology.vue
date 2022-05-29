158bb7e6177909c892663c95cd94c031eca4a075
LazezBZH <lazez.bzh@caramail.fr>
2022-05-29 12:36:35 +0200

    V4 techno part1 desk

---
+++ src/components/OneTechnology.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="data[i].name" class="technology">
+    <h3 class="role">THE TERMINOLOGY...</h3>
+    <h4 class="name">{{ data[i].name.toUpperCase() }}</h4>
+    <p class="description">
+      {{ data[i].description }}
+    </p>
+  </div>
+</template>
+
+<script>
+import json from "../data.json";
+let data = json.technology;
+console.log(data);
+
+export default {
+  name: "OneTechnology",
+  props: {
+    i: {},
+  },
+  data() {
+    return {
+      data,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.technology {
+  color: white;
+  width: 80%;
+  margin-left: 0;
+  margin-top: 0;
+}
+.image {
+  width: 10rem;
+}
+h3 {
+  font-family: "Noto Serif", serif;
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: rgba(255, 255, 255, 0.5);
+}
+h4 {
+  font-family: "Noto Serif", serif;
+  font-size: 2.62rem;
+  font-weight: 400;
+  margin: 1.3rem auto;
+}
+.description {
+  font-family: "Barlow", sans-serif;
+  font-size: 1.2rem;
+  margin: 1rem auto 2rem auto;
+  padding-right: 2.1rem;
+  color: rgba(208, 214, 249, 0.9);
+  line-height: 1.8rem;
+}
+</style>
--- src/views/TechnologyView.vue
+++ src/views/TechnologyView.vue
@@ -1,5 +1,99 @@
 <template>
-  <div class="technology">
-    <h1>This is an technology page</h1>
+  <div class="technologies">
+    <h1><span>02</span> SPACE LAUNCH 101</h1>
+    <div class="technology-txt">
+      <div class="technology-all">
+        <div class="btn">
+          <button
+            v-for="item in data"
+            :key="item.name"
+            v-on:click="activeId = data.indexOf(item)"
+          >
+            {{ data.indexOf(item) }}
+          </button>
+        </div>
+        <OneTechnology :i="activeId"></OneTechnology>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import json from "../data.json";
+let data = json.technology;
+
+console.log(data);
+import OneTechnology from "../components/OneTechnology";
+export default {
+  name: "TechnologyView",
+  components: { OneTechnology },
+  data() {
+    return {
+      data,
+      activeId: 0,
+    };
+  },
+};
+</script>
+<style scoped>
+.technologies {
+  max-width: 1440px;
+  padding-top: 17%;
+  width: 100%;
+  height: 100vh;
+  background-image: url("@/assets/technology/background-technology-desktop.jpg");
+  background-size: auto;
+  background-repeat: no-repeat;
+  background-position: top right;
+}
+h1 {
+  color: white;
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 1.7rem;
+  letter-spacing: 4.72px;
+  font-weight: 400;
+  margin-left: 10rem;
+}
+h1 span {
+  padding-right: 2rem;
+  color: rgba(255, 255, 255, 0.4);
+  font-weight: 700;
+}
+.technology-txt {
+  width: 57%;
+  height: 60vh;
+  margin-left: 0;
+  position: relative;
+}
+.technology-all {
+  width: 79.7%;
+  height: 60%;
+  position: absolute;
+  bottom: 22%;
+  right: 0;
+
+  display: flex;
+}
+.btn {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-right: 5rem;
+}
+button {
+  width: 5rem;
+  height: 5rem;
+  background-color: white;
+  color: black;
+  border: none;
+  padding: 0;
+  border-radius: 50%;
+  font-size: 2rem;
+}
+h2 {
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 1.5rem;
+  font-weight: 400;
+  letter-spacing: 2.7px;
+}
+</style>
